831c7fa0bab17b773f3410011d0901d6020c5ab7
osepoo <benardghali31@gmail.com>
2022-03-06 17:37:20 +0300

    Second Commit

--- linuxconfig.sh
+++ linuxconfig.sh
@@ -27,14 +27,15 @@ echo -e "\t2 - To check disk space"
 echo -e "\t3 - To check the calender"
 echo -e "\t4 - To display all the systems environment variables"
 echo -e "\t5 - To display the disk usage and the total in bytes"
-echo -e "\t6 - To exit"
+echo -e "\t6 - DNS lookup in human readable format"
+echo -e "\t7- To exit"
 read number;
 echo -e "================================================================================================"
 echo -e "\n"
 
 	performcmds()
 	{
-		if [[ $number -ne 6 ]]; then
+		if [[ $number -ne 7 ]]; then
 			case $number in
 
 				1) clear;;
@@ -42,6 +43,7 @@ echo -e "\n"
 				3) cal;;
 				4) env;;
 				5) du -c -b;;
+				6) dig -h;;
 
 			esac
 
@@ -50,7 +52,7 @@ echo -e "\n"
 		fi
 	}
 
-while [[ $number -ne 6 ]]; do
+while [[ $number -ne 7 ]]; do
 
 	echo -e "\n"
 	echo -e "\n================================================================================================"
@@ -62,7 +64,8 @@ while [[ $number -ne 6 ]]; do
 	echo -e "\t3 - To check the calender"
 	echo -e "\t4 - To display all the systems environment variables"
 	echo -e "\t5 - To display the disk usage and the total in bytes"
-	echo -e "\t6 - To exit"
+	echo -e "\t6 - DNS lookup in human readable format"
+	echo -e "\t7- To exit"
 	read number;
 	performcmds
 	echo -e "\n================================================================================================"
